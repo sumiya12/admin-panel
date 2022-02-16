25373ed29085b2c22d9de84ebdac08087a82bcf0
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-02-16 16:47:32 +0800

    [ADD] add more examples with date time and remove root package.json

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
-import { DatePicker } from 'antd';
+import './index.css';
+import 'antd/dist/antd.css';
+
+import React, { useState } from 'react';
+import { DatePicker, message } from 'antd';
 
 function App() {
+  const [date, setDate] = useState(null);
+  const handleChange = value => {
+    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+    setDate(value);
+  };
   return (
-    <>
-      <DatePicker />
-    </>
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+      </div>
+    </div>
   );
+  
 }
 
 export default App;
