15312f976df7cfc7f34c59e51b155ca99ce9501b
sumiya12 <sumiya.udval@gmail.com>
2022-06-20 00:12:46 +0800

    css

--- src/component/SideMenu/Orders.js
+++ src/component/SideMenu/Orders.js
@@ -2,6 +2,7 @@ import React, { useEffect,useState } from "react";
 // import { otherServices } from "../../services/otherServices";
 import { useOrder } from "../../contexts/OrderContext";
 import { useUser } from "../../contexts/UserContext";
+import moment from "moment";
 import { List, Row, Col, Divider, Pagination } from "antd";
 import "../../style/menuStyle/orders.css";
 export default function Orders(values ) {
@@ -32,7 +33,7 @@ export default function Orders(values ) {
       <Divider orientation="left">Захиалгууд</Divider>
       <List
         header={
-          <div className="header" lg={{ span: 2, offset: 1 }}>
+          <div className="header" style={{justifyContent:'space-between',display:'flex',paddingLeft:'10px' ,marginLeft:"0px"}} lg={{ span: 2, offset: 1 }}>
             <span>Он сар өдөр</span>
             <span>Захиалга #</span>
             <span>Хэрэглэгч</span>
@@ -49,30 +50,33 @@ export default function Orders(values ) {
         renderItem={(item) => {
           return (
             <>
-              <List.Item className="listItems">
-                <Row className="rows">
-                  <Col className="cols" lg={{ span: 2, offset: 1 }}>
-                    {item.created_date}
+              <List.Item className="listItems" style={{}}>
+                <Row className="rows" style={{width:'100vw',justifyContent:'space-between',display:'flex', }} >
+                  <Col className="cols" lg={{ span: 2, offset: 1 }}  xs={{ span: 1, offset: 1 }} style={{ padding:'0px' ,margin:"0px 10px 0px 10px"}} >
+                    {moment(item.created_date).format("YYYY/MM/DD")}
                   </Col>
-                  <Col className="cols" lg={{ span: 2, offset: 1 }}>
+                  <Col className="cols" lg={{ span: 2, offset: 1 }}  xs={{ span: 1, offset: 1 }} style={{ padding:'0px' ,margin:"0px 10px 0px 10px"}} >
                     {item.deliverman_id}
                   </Col>
-                  <Col className="cols" lg={{ span: 2, offset: 1 }}>
+                  <Col className="cols" lg={{ span: 2, offset: 1 }}  xs={{ span: 1, offset: 1 }} style={{ padding:'0px' ,margin:"0px 10px 0px 10px"}} >
                     {item.payment_type}
                   </Col>
-                  <Col className="cols" lg={{ span: 2, offset: 1 }}>
+                  <Col className="cols" lg={{ span: 2, offset: 1 }}  xs={{ span: 1, offset: 1 }} style={{ padding:'0px' ,margin:"0px 10px 0px 10px"}} >
                     {item.status}
                   </Col>
-                  <Col className="cols" lg={{ span: 2, offset: 1 }}>
+                  <Col className="cols" lg={{ span: 2, offset: 1 }}  xs={{ span: 1, offset: 1 }} style={{ padding:'0px' ,margin:"0px 10px 0px 10px"}}>
                     {item.total_price}
                   </Col>
-                  <Col className="cols" lg={{ span: 2, offset: 1 }}>
+                  <Col className="cols" lg={{ span: 2, offset: 1 }}  xs={{ span: 1, offset: 1 }} style={{ padding:'0px' ,margin:"0px 10px 0px 10px"}}>
                     {item.user_id}
                   </Col>
-                  <Col className="cols" lg={{ span: 2, offset: 1 }}>
+                  <Col className="cols" lg={{ span: 2, offset: 1 }}  xs={{ span: 1, offset: 1 }} style={{ padding:'0px' ,margin:"0px 10px 0px 10px"}}>
                     {item._v}
                   </Col>
-                  <Col className="cols" lg={{ span: 2, offset: 1 }}>
+                  <Col className="cols" lg={{ span: 2, offset: 1 }}  xs={{ span: 1, offset: 1 }} style={{ padding:'0px' ,margin:"0px 10px 0px 10px"}}>
+                    {item.phone}
+                  </Col>
+                  <Col className="cols" lg={{ span: 2, offset: 1 }}  xs={{ span: 1, offset: 1 }} style={{ padding:'0px' ,margin:"0px 10px 0px 10px"}}>
                     {item._id}
                   </Col>
                 </Row>
